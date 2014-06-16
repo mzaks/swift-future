6b327220ae992b5105734f0836d76bbf0e9ab9ac
Maxim Zaks <maxim.zaks@gmail.com>
2014-06-16 17:15:04 +0200

    Initial Commit

---
+++ Future/Future.h
@@ -0,0 +1,19 @@
+//
+//  Future.h
+//  Future
+//
+//  Created by Maxim Zaks on 16.06.14.
+//  Copyright (c) 2014 Maxim Zaks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Future.
+FOUNDATION_EXPORT double FutureVersionNumber;
+
+//! Project version string for Future.
+FOUNDATION_EXPORT const unsigned char FutureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Future/PublicHeader.h>
+
+
